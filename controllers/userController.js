d06b4f4be575a8824af40813264d1eb398ca5843
David Dyer <davidrossdyer@gmail.com>
2022-01-04 14:47:02 -0500

    added user and thought routes + controllers

---
+++ controllers/userController.js
@@ -0,0 +1,106 @@
+const { User, Thought } = require("../models");
+
+// TODO: Create an aggregate function to get the number of users overall
+const userTotal = async () =>
+  User.aggregate().then((numberOfUsers) => numberOfUsers);
+
+module.exports = {
+  // Get all users
+  getUsers(req, res) {
+    User.find()
+      .then(async (users) => {
+        const userObj = {
+          users,
+          userTotal: await userTotal(),
+        };
+        return res.json(userObj);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+
+  // Get a single user
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select("-__v")
+      .lean()
+      .then(async (user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : res.json({
+              user,
+              thought: await thought(req.params.userId),
+            })
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+
+  // create a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Delete a user and their thoughts
+  deleteUser(req, res) {
+    User.findOneAndRemove({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No such user exists" })
+          : Thought.findOneAndUpdate(
+              { users: req.params.userId },
+              { $pull: { users: req.params.userId } },
+              { new: true }
+            )
+      )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({
+              message: "User deleted, but no thoughts found",
+            })
+          : res.json({ message: "User successfully deleted" })
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  // Add a thought to a user
+  addThought(req, res) {
+    console.log("You are adding a thought");
+    console.log(req.body);
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { thoughts: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user found with that ID :(" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Remove thought from a user
+  removeThought(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { thought: { thoughtId: req.params.thoughtId } } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user found with that ID :(" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  deleteUser,
+  addThought,
+  removeThought,
+} = require("../../controllers/userController");
+
+// /api/users
+router.route("/").get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route("/:userId").get(getSingleUser).delete(deleteUser);
+
+// /api/users/:userId/thoughts
+router.route("/:userId/thoughts").post(addThought);
+
+// /api/users/:userId/thoughts/:thoughtId
+router.route("/:userId/assignments/:thoughtId").delete(removeThought);
+
+module.exports = router;
