1c1b7eccc9c0b41c736eed39b00eb478baa2f88d
David Dyer <davidrossdyer@gmail.com>
2022-01-07 12:54:25 -0500

    all user routes working

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -3,93 +3,94 @@ const { Thought, User } = require("../models");
 module.exports = {
   // Get all thoughts
   getThoughts(req, res) {
-    Thought.find()
+    Thought.find({})
       .then((thoughts) => res.json(thoughts))
       .catch((err) => res.status(500).json(err));
+    console.log(thoughts);
   },
 
   // Get a thought
-  getSingleThought(req, res) {
-    Thought.findOne({ _id: req.params.thoughtId })
-      .select("-__v")
-      .then((thought) =>
-        !thought
-          ? res.status(404).json({ message: "No thought with that ID" })
-          : res.json(thought)
-      )
-      .catch((err) => res.status(500).json(err));
-  },
+  // getSingleThought(req, res) {
+  //   Thought.findOne({ _id: req.params.thoughtId })
+  //     .select("-__v")
+  //     .then((thought) =>
+  //       !thought
+  //         ? res.status(404).json({ message: "No thought with that ID" })
+  //         : res.json(thought)
+  //     )
+  //     .catch((err) => res.status(500).json(err));
+  // },
 
-  // Create a thought
-  createThought(req, res) {
-    Thought.create(req.body)
-      .then((thought) => res.json(thought))
-      .catch((err) => {
-        console.log(err);
-        return res.status(500).json(err);
-      });
-  },
+  // // Create a thought
+  // createThought(req, res) {
+  //   Thought.create(req.body)
+  //     .then((thought) => res.json(thought))
+  //     .catch((err) => {
+  //       console.log(err);
+  //       return res.status(500).json(err);
+  //     });
+  // },
 
-  // Delete a thought
-  deleteThought(req, res) {
-    Thought.findOneAndDelete({ _id: req.params.thoughtId })
-      .then((thought) =>
-        !thought
-          ? res.status(404).json({ message: "No thought with that ID" })
-          : User.deleteMany({ _id: { $in: thought.users } })
-      )
-      .then(() => res.json({ message: "Thought and user deleted!" }))
-      .catch((err) => res.status(500).json(err));
-  },
+  // // Delete a thought
+  // deleteThought(req, res) {
+  //   Thought.findOneAndDelete({ _id: req.params.thoughtId })
+  //     .then((thought) =>
+  //       !thought
+  //         ? res.status(404).json({ message: "No thought with that ID" })
+  //         : User.deleteMany({ _id: { $in: thought.users } })
+  //     )
+  //     .then(() => res.json({ message: "Thought and user deleted!" }))
+  //     .catch((err) => res.status(500).json(err));
+  // },
 
-  // Update a thought
-  updateThought(req, res) {
-    Thought.findOneAndUpdate(
-      { _id: req.params.thoughtId },
-      { $set: req.body },
-      { runValidators: true, new: true }
-    )
-      .then((thought) =>
-        !thought
-          ? res.status(404).json({ message: "No thought with this id!" })
-          : res.json(thought)
-      )
-      .catch((err) => res.status(500).json(err));
-  },
+  // // Update a thought
+  // updateThought(req, res) {
+  //   Thought.findOneAndUpdate(
+  //     { _id: req.params.thoughtId },
+  //     { $set: req.body },
+  //     { runValidators: true, new: true }
+  //   )
+  //     .then((thought) =>
+  //       !thought
+  //         ? res.status(404).json({ message: "No thought with this id!" })
+  //         : res.json(thought)
+  //     )
+  //     .catch((err) => res.status(500).json(err));
+  // },
 
-  // Add a reaction to a thought
-  addReaction(req, res) {
-    console.log("You are adding a reaction");
-    console.log(req.body);
-    Thought.findOneAndUpdate(
-      { _id: req.params.thoughtId },
-      { $addToSet: { reactions: req.body } },
-      { runValidators: true, new: true }
-    )
-      .then((thought) =>
-        !thought
-          ? res
-              .status(404)
-              .json({ message: "No thought found with that ID :(" })
-          : res.json(thought)
-      )
-      .catch((err) => res.status(500).json(err));
-  },
+  // // Add a reaction to a thought
+  // addReaction(req, res) {
+  //   console.log("You are adding a reaction");
+  //   console.log(req.body);
+  //   Thought.findOneAndUpdate(
+  //     { _id: req.params.thoughtId },
+  //     { $addToSet: { reactions: req.body } },
+  //     { runValidators: true, new: true }
+  //   )
+  //     .then((thought) =>
+  //       !thought
+  //         ? res
+  //             .status(404)
+  //             .json({ message: "No thought found with that ID :(" })
+  //         : res.json(thought)
+  //     )
+  //     .catch((err) => res.status(500).json(err));
+  // },
 
-  // Remove reaction from a thought
-  deleteReaction(req, res) {
-    Thought.findOneAndUpdate(
-      { _id: req.params.thoughtId },
-      { $pull: { reaction: { reactionId: req.params.reactionId } } },
-      { runValidators: true, new: true }
-    )
-      .then((thought) =>
-        !thought
-          ? res
-              .status(404)
-              .json({ message: "No thought found with that ID :(" })
-          : res.json(thought)
-      )
-      .catch((err) => res.status(500).json(err));
-  },
+  // // Remove reaction from a thought
+  // deleteReaction(req, res) {
+  //   Thought.findOneAndUpdate(
+  //     { _id: req.params.thoughtId },
+  //     { $pull: { reaction: { reactionId: req.params.reactionId } } },
+  //     { runValidators: true, new: true }
+  //   )
+  //     .then((thought) =>
+  //       !thought
+  //         ? res
+  //             .status(404)
+  //             .json({ message: "No thought found with that ID :(" })
+  //         : res.json(thought)
+  //     )
+  //     .catch((err) => res.status(500).json(err));
+  // },
 };
--- utils/seed.js
+++ utils/seed.js
@@ -1,5 +1,6 @@
 const connection = require("../config/connection");
 const { Thought, User } = require("../models");
+const { seedUsers, seedThoughts } = require("./data");
 
 connection.on("error", (err) => err);
 
@@ -12,151 +13,8 @@ connection.once("open", async () => {
   // Drop existing thoughts
   await Thought.deleteMany({});
 
-  // Empty array to hold the users
-  // const users = [];
-
-  // User seed
-  const seedUsers = [
-    {
-      username: "Carlos Avalos",
-      email: "carlos.avalos@gmail.com",
-    },
-    {
-      username: "Daniel Awad",
-      email: "dwad@yahoo.com",
-    },
-    {
-      username: "Mike Barajas",
-      email: "m.barajas@cox.net",
-    },
-    {
-      username: "Eddie Dominguez",
-      email: "eddie@bzerk.com",
-    },
-    {
-      username: "Johnny Chang",
-      email: "johnnyc123@hotmail.com",
-    },
-    {
-      username: "Josh Clements",
-      email: "josh.clements@gmail.com",
-    },
-    {
-      username: "Mike Cox",
-      email: "mike.c@futurestreet.com",
-    },
-    {
-      username: "Benjie Escobar",
-      email: "benjie@hotmail.com",
-    },
-    {
-      username: "Adam Gonzales",
-      email: "adam_g@clsc.com",
-    },
-    {
-      username: "Dillon Jones",
-      email: "dillon.jones@tripleseven.com",
-    },
-    {
-      username: "Brien Morgan",
-      email: "sosoanxious@gmail.com",
-    },
-    {
-      username: "Jasmine Ortega",
-      email: "jasmine@vans.com",
-    },
-    {
-      username: "Ronald Ortiz",
-      email: "ronald@thehundreds.com",
-    },
-    {
-      username: "Tony Ramirez",
-      email: "djynot@hotmail.com",
-    },
-    {
-      username: "David Rivera",
-      email: "david.rivera@stussy.com",
-    },
-  ];
-
   // Add users to the collection and await the results
   await User.collection.insertMany(seedUsers);
-
-  // Empty array to hold user thoughts
-  // const thoughts = [];
-
-  // Thought seed
-  const seedThoughts = [
-    {
-      thoughtText:
-        "This damn pandemic has gone on long enough! Get vaccinated you idiots!",
-      username: "David Rivera",
-    },
-    {
-      thoughtText:
-        "Dr. Fauci is a fraud, the virus is a biological weapon from China!",
-      username: "Tony Ramirez",
-    },
-    {
-      thoughtText:
-        "As long as there is money in politics, nothing will ever get done for my people",
-      username: "Ronald Ortiz",
-    },
-    {
-      thoughtText: "Check out this cute cat video OMG!",
-      username: "Jasmine Ortega",
-    },
-    {
-      thoughtText:
-        "This platform is much better than Facebook, who needs a Metaverse mansion anyway",
-      username: "Brien Morgan",
-    },
-    {
-      thoughtText: "Free Julian Assange!",
-      username: "Dillon Jones",
-    },
-    {
-      thoughtText:
-        "Hope everyone is having a great new year! Fingers crossed for the salvation of humanity and all!",
-      username: "Adam Gonzales",
-    },
-    {
-      thoughtText: "Name your best album of 2021 below..  GO!",
-      username: "Benjie Escobar",
-    },
-    {
-      thoughtText:
-        "Just landed the job with Google! Now to pay off that pesky 150k student loan.",
-      username: "Mike Cox",
-    },
-    {
-      thoughtText:
-        "Recycling is broken in the United State, we need a new deal solution to plastic waste",
-      username: "Josh Clements",
-    },
-    {
-      thoughtText: "Our webshop is now restocked with new items, link in bio!",
-      username: "Johnny Chang",
-    },
-    {
-      thoughtText: "Look at how awesome my dog is!",
-      username: "Mike Barajas",
-    },
-    {
-      thoughtText:
-        "Hello friends! I'm moving this spring, so let's get togther soon! Miss you all.",
-      username: "Carlos Avalos",
-    },
-    {
-      thoughtText: "At this point it should be called the fight for $25",
-      username: "Eddie Dominguez",
-    },
-    {
-      thoughtText:
-        "We are trying to reach you about your car's extended warranty.",
-      username: "Daniel Awad",
-    },
-  ];
 
   // Add thoughts to the collection and await the results
   await Thought.collection.insertMany(seedThoughts);
